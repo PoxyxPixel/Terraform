c1061b2aa90400e0e121f6742e9f1e3b99178a03
PoxyPixel <andreistana2006@gmail.com>
2022-01-08 12:00:17 +0200

    SettingRequired Providers

--- Terraform AWS/Main.tf
+++ Terraform AWS/Main.tf
@@ -16,8 +16,16 @@
 #Terra ver check -v
 terraform {
   required_version = ">= 0.12"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
-
+provider "aws" {
+  region = "eu-central-1"
+}
 
 #VPC
 resource "aws_vpc" "VPCEmmaX"{
